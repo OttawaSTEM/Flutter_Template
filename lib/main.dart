87918289d5d8bef084f0523232759b95dc1b01e5
OttawaSTEM <ottawastem@gmail.com>
2020-10-02 11:38:23 -0400

    tune emulator

--- lib/main.dart
+++ lib/main.dart
@@ -90,48 +90,48 @@ class _HomePageState extends State<HomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Container (
-          child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(onPressed: () {
-              print('Button Clicked!');
-              _getData();
-            },
-              child: Text('Click me'),
-              color: Colors.lightBlue,
-            ),
-            Text(
-              'You have pushed the button this many times:',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+          child: Column (
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              RaisedButton(onPressed: () {
+                print('Button Clicked!');
+                _getData();
+              },
+                child: Text('Click me'),
+                color: Colors.lightBlue,
               ),
-            ),
-            Text(
-              '$_counter',
-              // style: Theme.of(context).textTheme.headline4,
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.red[600],
+              Text(
+                'You have pushed the button this many times:',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
-        ),
+              Text(
+                '$_counter',
+                // style: Theme.of(context).textTheme.headline4,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red[600],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
 
